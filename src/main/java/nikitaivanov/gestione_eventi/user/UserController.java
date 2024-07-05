18397e3299d3eaf51566f9f1511812355034936f
FocusFine89 <niivanov02@gmail.com>
2024-07-05 15:24:36 +0200

    Creazione User Controller

---
+++ src/main/java/nikitaivanov/gestione_eventi/user/UserController.java
@@ -0,0 +1,18 @@
+package nikitaivanov.gestione_eventi.user;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/me")
+    public User profile(@AuthenticationPrincipal User currentUser){
+        return currentUser;
+    }
+
+
+}
